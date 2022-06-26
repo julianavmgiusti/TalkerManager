35a72786fcf7b40cc8e65b5283ea19406002e6ba
Juliana Giusti <juliana.vmelo@gmail.com>
2022-06-25 22:05:06 -0300

    feat: const for path talkers file and create talker by id route

--- routes/router.js
+++ routes/router.js
@@ -3,12 +3,23 @@ const express = require('express');
 const router = express.Router();
 const { readFile } = require('../helpers/readWrite');
 
+const PATH_TALKERSFILE = './talker.json';
+
 router.get('/', async (_req, res) => {
-  const talker = await readFile('./talker.json');
-  if (!talker || talker === undefined) res.status(200).json(JSON.parse([]));
-  res.status(200).json(JSON.parse(talker));
+  const talkers = await readFile(PATH_TALKERSFILE);
+  if (!talkers || talkers === undefined) res.status(200).json(JSON.parse([]));
+  res.status(200).json(JSON.parse(talkers));
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readFile(PATH_TALKERSFILE);
+    const talker = JSON.parse(talkers).find((e) => e.id === Number(id));
+    if (!talker || talker === undefined) {
+      res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    res.status(200).json(talker);
+  });
 module.exports = {
   router,
 }; 
